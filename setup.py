8f7ff85f17946b1a02c49600ad74c7ec0e1781f8
Ebubechukwu Ibeh <bgraphixx5@gmail.com>
2023-08-17 18:20:05 +0100

    New new

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("./README.md", "r") as f:
+    long_description = f.read()
 
 setup(
     name='safetokengen',
-    version='0.1',
+    version='0.0.1',
     description='A package for generating secure API keys, passwords, tokens, OTPs, and PINs.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Ebubechukwu Ibeh',
     author_email='bgraphixx5@gmail.com',
     url='https://github.com/bgraphixx/safetokengen',
     packages=['safetokengen'],
     install_requires=[],
+    license='MIT',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
@@ -18,5 +24,6 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        "Operating System :: OS Independent",
     ],
 )
